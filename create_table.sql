781597750ae2e067330330d8b8e7cf8731ce0ffe
Yener Tuz <32311607+yenertuz@users.noreply.github.com>
2018-11-06 19:45:56 -0800

    update create_table.sql

--- create_table.sql
+++ create_table.sql
@@ -1,7 +1,7 @@
 use yenertuz;
 create table if not exists employees
-( id INT NOT_NULL,
-name varchar(255),
-department varchar(255),
-salary int,
+( id INT not null auto_increment,
+name varchar(255) default '',
+department varchar(255) default '',
+salary int default 0,
 primary key(id));
