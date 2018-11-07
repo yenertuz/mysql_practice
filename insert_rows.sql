cef6b19b808b5d2d8c01ad118d08cc3a2a9abbf3
Yener Tuz <32311607+yenertuz@users.noreply.github.com>
2018-11-06 19:48:10 -0800

    create insert_rows.sql

---
+++ insert_rows.sql
@@ -0,0 +1,8 @@
+use yenertuz;
+insert into employees (name, department, salary)
+values ('Yener', 'IT', 3000),
+('Melek', 'Management', 5000),
+('Mert', 'Health', 9000),
+('Art', 'IT', 1000),
+('Angel', 'Health', 2000),
+('Sevim', 'Management', 2500);
